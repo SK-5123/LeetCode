c9b9ce803768ff81e357a4fb402c1cf2386a60f2
Saurav <114897038+SK-5123@users.noreply.github.com>
2024-02-19 21:32:05 +0530

    Add files via upload

---
+++ 45. Jump Game II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+         int n = nums.size();
+        if (n <= 1) return 0;
+        int jumps = 0;
+        int maxReach = 0;
+        int steps = 0;
+        
+        for (int i = 0; i < n - 1; ++i) {
+            maxReach = max(maxReach, i + nums[i]);
+            if (i == steps) { 
+                ++jumps;
+                steps = maxReach; 
+            }
+        }
+        
+        return jumps;
+    }
+};
