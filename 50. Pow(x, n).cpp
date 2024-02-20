e451fc74fdbb6a28f3ddcd80b60cc66220c26dc6
Saurav <114897038+SK-5123@users.noreply.github.com>
2024-02-20 23:16:11 +0530

    Add files via upload

---
+++ 50. Pow(x, n).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+         if (n == 0) return 1.0; 
+        if (n == 1) return x; 
+        if (n < 0) {
+            x = 1 / x;
+            if (n == INT_MIN) {
+                return x * myPow(x, -(n + 1));
+            }
+            n = -n;
+        }
+        double half = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half; 
+        } else {
+            return x * half * half; 
+        }
+    }
+};
